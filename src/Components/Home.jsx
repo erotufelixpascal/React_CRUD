4ba835492d7c4e068c56fa83e3932f1ca099348e
FelixE <erotufelixpascal2@gmail.com>
2023-11-29 13:36:24 +0300

    NewFeatures

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.css';
+import { Link } from "react-router-dom";
 
 
 function Home() {
@@ -16,6 +17,9 @@ function Home() {
         <div className='d-flex flex-column justify-content-center align-items-center bg-light vh-100'>
             <h1>List of Users</h1>
             <div className='w-75 rounded bg-white border shadow p-4'>
+                <div className='d-flex justify content-end'>
+                    <Link to ="/create" className='btn btn-success'>Add +</Link>
+                </div>
                 <table className='table table-striped'>
                     <thead>
                         <tr>
@@ -35,7 +39,7 @@ function Home() {
                                     <td>{d.email}</td>
                                     <td>{d.phone}</td>
                                     <td>
-                                        <button className='btn btn-sm btn-primary me-2'>Add</button>
+                                        <button className='btn btn-sm btn-primary me-2'>Read</button>
                                         <button className='btn btn-sm btn-primary me-2'>Edit</button>
                                         <button className='btn btn-sm btn-danger'>Delete</button>
                                     </td>
