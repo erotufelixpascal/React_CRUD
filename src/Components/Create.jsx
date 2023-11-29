2f24155b5015e4b9091bbb524a5861f554536f8c
FelixE <erotufelixpascal2@gmail.com>
2023-11-29 13:44:39 +0300

    NewFeatures

--- src/Components/Create.jsx
+++ src/Components/Create.jsx
@@ -2,7 +2,29 @@ import React from "react";
 
 function Create() {
     return ( 
-        <div>Create</div>
+        <div className='d-flex w-100 vh-100 justify-center-center align-items-center gb-light'>
+            <div className='w-50 border gb-white shadow px-5 pt-3 pb-5 rounded'>
+                <h1>Add a User</h1>
+                <form>
+                    <div className='mb-2'>
+                        <label htmlFor="name">Name:</label>
+                        <input type="text" name="name" className="form-control" placeholder="Enter Name" />
+                    </div>
+                    <div className='mb-2'>
+                        <label htmlFor="name">Email:</label>
+                        <input type="email" name="email" className="form-control" placeholder="Enter Email" />
+                    </div>
+                    <div className='mb-2'>
+                        <label htmlFor="name">Phone:</label>
+                        <input type="text" name="phone" className="form-control" placeholder="Enter phone" />
+                    </div>
+                    <div className='mb-2'>
+                        <label htmlFor="name">Phone:</label>
+                        <input type="text" name="phone" className="form-control" placeholder="Enter phone" />
+                    </div>
+                </form>
+            </div>
+        </div>
      );
 }
 
