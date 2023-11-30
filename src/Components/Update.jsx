f7e18db6dad95e1ef97d29c51a699aaa9c37acf9
FelixE <erotufelixpascal2@gmail.com>
2023-11-30 15:51:52 +0300

    UpdatePage

--- src/Components/Update.jsx
+++ src/Components/Update.jsx
@@ -5,27 +5,29 @@ import 'bootstrap/dist/css/bootstrap.css';
 
 function Update() {
     return ( 
-        <div>
+        <div className="d-flex w-100 vh-100 justify-content-center align-items-center bg-light">
+            <div className="w-50 border bg-white shadow px-5 pt-3 pb-5 rounded">
             <h1>Update User Information</h1>
-                <form onSubmit={handleSubmit}>
+                <form >
                     <div className='mb-2'>
                         <label htmlFor="name">Name:</label>
                         <input type="text" name="name" className="form-control" placeholder="Enter Name" 
-                        onchange = {e => setValues({...values, name: e.target.value})}/>
+                        />
                     </div>
                     <div className='mb-2'>
                         <label htmlFor="name">Email:</label>
                         <input type="email" name="email" className="form-control" placeholder="Enter Email" 
-                        onchange = {e => setValues({...values, email: e.target.value})}/>
+                        />
                     </div>
                     <div className='mb-2'>
                         <label htmlFor="name">Phone:</label>
                         <input type="text" name="phone" className="form-control" placeholder="Enter phone" 
-                        onchange = {e => setValues({...values, phone: e.target.value})} />
+                        />
                     </div>
                     <button className="">Submit</button>
                     <Link to="/" className="btn btn-primary ms-3">Back</Link>
                 </form>
+            </div>
         </div>
         
 )}
