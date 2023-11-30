6ae69d15a731f8c3afeefeb94dbe99c61fb49575
FelixE <erotufelixpascal2@gmail.com>
2023-11-30 14:26:01 +0300

    NewFeature

--- src/Components/Create.jsx
+++ src/Components/Create.jsx
@@ -15,17 +15,20 @@ function Create() {
                     <div className='mb-2'>
                         <label htmlFor="name">Name:</label>
                         <input type="text" name="name" className="form-control" placeholder="Enter Name" />
+                        onchange = {e => setValues({...values, name: e.target.value})}
                     </div>
                     <div className='mb-2'>
                         <label htmlFor="name">Email:</label>
                         <input type="email" name="email" className="form-control" placeholder="Enter Email" />
+                        onchange = {e => setValues({...values, email: e.target.value})}
                     </div>
                     <div className='mb-2'>
                         <label htmlFor="name">Phone:</label>
                         <input type="text" name="phone" className="form-control" placeholder="Enter phone" />
+                        onchange = {e => setValues({...values, phone: e.target.value})}
                     </div>
                     <button className="">Submit</button>
-                    <Link to="/" className="btn-btn-primary ms-3">Back</Link>
+                    <Link to="/" className="btn btn-primary ms-3">Back</Link>
                 </form>
             </div>
         </div>
