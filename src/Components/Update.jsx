b0c88e9c90cc9f495ca438e68da2f6107ab5aa4e
FelixE <erotufelixpascal2@gmail.com>
2023-11-30 16:00:49 +0300

    UpdatePage

--- src/Components/Update.jsx
+++ src/Components/Update.jsx
@@ -1,9 +1,21 @@
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React from "react";
+import { Link, useParams } from "react-router-dom";
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.css';
+import { useEffect, useState } from "react";
+
 
 function Update() {
+    //read to the data
+    const [data ,setData] = useState ([])
+    const {id} =useParams([])
+    useEffect (() =>{
+        axios.get('http://localhost:3000/Users' + id)
+        .then(res => setData(res.data))
+        .catch(err => console.log(err));
+
+    }, [])
+
     return ( 
         <div className="d-flex w-100 vh-100 justify-content-center align-items-center bg-light">
             <div className="w-50 border bg-white shadow px-5 pt-3 pb-5 rounded">
@@ -12,17 +24,17 @@ function Update() {
                     <div className='mb-2'>
                         <label htmlFor="name">Name:</label>
                         <input type="text" name="name" className="form-control" placeholder="Enter Name" 
-                        />
+                        value= {data.name} />
                     </div>
                     <div className='mb-2'>
                         <label htmlFor="name">Email:</label>
                         <input type="email" name="email" className="form-control" placeholder="Enter Email" 
-                        />
+                        value= {data.email}/>
                     </div>
                     <div className='mb-2'>
                         <label htmlFor="name">Phone:</label>
                         <input type="text" name="phone" className="form-control" placeholder="Enter phone" 
-                        />
+                        value= {data.phone}/>
                     </div>
                     <button className="">Submit</button>
                     <Link to="/" className="btn btn-primary ms-3">Back</Link>
